6caaf083f4aee398e2df1b359ff93b3ee3a87dc1
shizg <shizengguang@landleaf-tech.com>
2020-05-12 18:03:28 +0800

    多数据源以及切换

--- my-datasource/src/main/java/com/shizg/smartme/dynamic/ContextDatasourceTypeHolder.java
+++ my-datasource/src/main/java/com/shizg/smartme/dynamic/ContextDatasourceTypeHolder.java
@@ -6,7 +6,17 @@ package com.shizg.smartme.dynamic;
 * @date 2017/8/8 14:40
 * @version V1.0   
 */
-public class ContextDatasourceTypeHolder {
+
+/**
+ * JDK1.7开始引入
+ * 对于实现AutoCloseable接口的类的实例，将其放到try后面（我们称之为：带资源的try语句），在try结束的时候，会自动将这些资源关闭（调用close方法）。
+ *
+ * 带资源的try语句的3个关键点：
+ * 1、由带资源的try语句管理的资源必须是实现了AutoCloseable接口的类的对象。
+ * 2、在try代码中声明的资源被隐式声明为fianl。
+ * 3、通过使用分号分隔每个声明可以管理多个资源
+ */
+public class ContextDatasourceTypeHolder implements AutoCloseable{
 
     private static ThreadLocal<String> datasourceTypeHolder = new ThreadLocal<String>();
 
@@ -14,17 +24,18 @@ public class ContextDatasourceTypeHolder {
      * 甚至当前操作的数据源类型
      * @param dataSourceType
      * @author
-     * @date 2017年08月08日14:42:59
      */
     public static void setDatasourceType(String dataSourceType) {
         datasourceTypeHolder.set(dataSourceType);
     }
 
+    public ContextDatasourceTypeHolder(String key) {
+        datasourceTypeHolder.set(key);
+    }
     /**
      * 获取当前操作的数据源类型
      * @return
      * @author
-     * @date 2017年08月08日14:43:15
      */
     public static String getDatasourceType() {
         return datasourceTypeHolder.get();
@@ -33,10 +44,13 @@ public class ContextDatasourceTypeHolder {
     /**
      * 清除当前操作的数据源类型，使用默认的了
      * @author
-     * @date 2017年08月08日14:43:38
      */
     public static void clearDatasourceType() {
         datasourceTypeHolder.remove();
     }
 
+    @Override
+    public void close() throws Exception {
+        datasourceTypeHolder.remove();
+    }
 }
--- my-datasource/src/main/java/com/shizg/smartme/dynamic/DatasourceChangeAspect.java
+++ my-datasource/src/main/java/com/shizg/smartme/dynamic/DatasourceChangeAspect.java
@@ -26,29 +26,19 @@ public class DatasourceChangeAspect {
      * @author
      * @date 2017年08月08日15:04:28
      */
-    @Around("@annotation(com.shizg.smartme.dynamic.AnotherDatasource)")
-    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
-        try {
-            //获取目标类
-            Class<?> targetClazz = joinPoint.getTarget().getClass();
-            //获取执行的方法名
-            String methodName = joinPoint.getSignature().getName();
-            //获取此方法的参数列表
-            Class<?>[] par = ((MethodSignature) joinPoint.getSignature()).getParameterTypes();
-            //获取到实际执行的方法
-            Method objMethod = targetClazz.getMethod(methodName, par);
-            //从执行的方法上获取注解
-            AnotherDatasource annotation = objMethod.getAnnotation(AnotherDatasource.class);
-            if (annotation != null) {
-                //将注解上指定的数据源类型读取到
-                String datasourceType = annotation.value();
-                //设置当前处理的数据源类型为注解上指定的
-                ContextDatasourceTypeHolder.setDatasourceType(datasourceType);
-            }
-            Object result = joinPoint.proceed();
-            return result;
-        } finally {
-            ContextDatasourceTypeHolder.clearDatasourceType();
+
+    /**
+     *注意方法的第二个参数RoutingWith是Spring传入的注解实例，我们根据注解的value()获取配置的key。编译前需要添加一个Maven依赖
+     * <dependency>
+     *     <groupId>org.springframework.boot</groupId>
+     *     <artifactId>spring-boot-starter-aop</artifactId>
+     * </dependency>
+     */
+    @Around("@annotation(anotherDatasource)")
+    public Object process(ProceedingJoinPoint joinPoint,AnotherDatasource anotherDatasource) throws Throwable {
+        String source = anotherDatasource.value();
+        try (ContextDatasourceTypeHolder ctx = new ContextDatasourceTypeHolder(source)) {
+            return joinPoint.proceed();
         }
     }
 
